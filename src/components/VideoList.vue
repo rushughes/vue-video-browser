4850187be86227765750954338c339c878be594d
Rus Hughes <russell.hughes@gmail.com>
2019-10-19 11:11:25 +0200

    s3 l55

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,29 @@
 <template>
   <ul>
-    VideoList
+    <VideoListItem
+      v-for="video in videos"
+      :key="video.etag"
+      :video="video"
+    />
   </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
   name: 'VideoList',
+  components: {
+    VideoListItem
+  },
+  props: {
+    videos: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    }
+  }
 };
 </script>
 
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    {{ video.snippet.title }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: {
+    video: {
+      type: Object,
+      default: function() {
+        return null;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
