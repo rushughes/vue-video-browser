ecd82cf235dbd1f111a1fd8eee07a603768c8353
Rus Hughes <russell.hughes@gmail.com>
2019-10-19 09:52:13 +0200

    s3 l44

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange" />
   </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar';
+const API_KEY = process.env.API_KEY;
 
 export default {
   name: 'App',
   components: {
     SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      console.log(searchTerm);
+    }
   }
 }
 </script>
